450d7b7b907020cc896c60a6f8afb3188e9997ee
Inge Garcia <angel_rgg@hotmail.com>
2024-10-17 21:06:03 -0600

    animations and crypto

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class SaveSystem
+{
+    private static readonly string savePath = Application.persistentDataPath + "/game.save";
+
+    public static void SaveGame(GameData data)
+    {
+        string json_data = JsonUtility.ToJson(data);
+        string encryptedData = AESMethod.EncryptText(json_data);
+        File.WriteAllText(savePath, encryptedData);
+        PlayerPrefs.SetString("Date", DateTime.Now.ToString());
+    }
+
+    public static GameData LoadGame()
+    {
+        if (File.Exists(savePath))
+        {
+            string encryptedData = File.ReadAllText(savePath);
+            string decryptedJson = AESMethod.DecryptText(encryptedData);
+            return JsonUtility.FromJson<GameData>(decryptedJson);
+        }
+        else
+        {
+            Debug.LogError("No save file found!");
+            return null;
+        }
+    }
+}
