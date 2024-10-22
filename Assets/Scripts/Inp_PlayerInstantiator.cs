8e2987ae4bf79553511252cdddaef3f81da298c8
GabrielRuizMiller <110938299+GabrielRuizMiller@users.noreply.github.com>
2024-10-21 18:29:22 -0600

    NetcodeImportado y PlayerPrefabCorregido

--- Assets/Scripts/Inp_PlayerInstantiator.cs
+++ Assets/Scripts/Inp_PlayerInstantiator.cs
@@ -6,17 +6,11 @@ public class Inp_PlayerInstantiator : MonoBehaviour
 {
     [SerializeField] private GameObject[] characterObj;
 
-    [HideInInspector] public bool jumpButtonPressed;
 
     void Start()
     {
-        int amountPlayers = FindObjectsOfType<Inp_PlayerInstantiator>().Length;
-        characterObj[amountPlayers - 1].SetActive(true);
-        Map_Display_Boundaries.Instance.AddPlayer(characterObj[amountPlayers - 1]);
+        Map_Display_Boundaries.Instance.AddPlayer(this.gameObject);
     }
 
-    private void OnJump()
-    {
-        jumpButtonPressed = !jumpButtonPressed;
-    }
+    
 }
