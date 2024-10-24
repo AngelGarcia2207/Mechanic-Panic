3423ff6ad465d86508ef14a74678bc7711b35eda
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2024-10-24 12:29:36 -0600

    State Machine del robot chiquito

---
+++ Assets/Scripts/Enemies/States/IAs_ChargeForward_Long.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAs_ChargeForward_Long<EnemyState> : IAs_Enemy_State<EnemyState> where EnemyState : Enum
+{
+    public IAs_ChargeForward_Long(EnemyState charge, EnemyState idle, EnemyState stun, float speed) : base(charge)
+    {
+        idleStateKey = idle;
+        stunStateKey = stun;
+        chargeStrenght = speed;
+    }
+
+    protected EnemyState idleStateKey;
+    protected EnemyState stunStateKey;
+    protected GameObject[] activePlayers;
+    protected Transform targetedPlayer;
+    protected Vector3 fixedTarget;
+    protected float chargeStrenght;
+    protected float sideFactor = 1;
+    protected bool isPreparing, isCharging, isResting;
+
+    public override void EnterState(IAs_Enemy_State_Machine<EnemyState> SM)
+    {   
+        stateMachine = SM;
+        activePlayers = stateMachine.GetActivePlayers();
+        stateMachine.waitForTime.AddListener(OnWaitOver);
+        stateMachine.StartCoroutine("WaitTime", 1f);
+        targetedPlayer = activePlayers[0].transform;
+
+        for(int i = 1; i < activePlayers.Length; i++)
+        {
+            if(Vector3.Distance(activePlayers[i].transform.position, stateMachine.GetEnemyTransform().position)
+                < Vector3.Distance(targetedPlayer.position, stateMachine.GetEnemyTransform().position))
+            {
+                targetedPlayer = activePlayers[i].transform;
+            }
+        }
+
+        isPreparing = true;
+        isCharging = true;
+        isResting = true;
+    }
+
+    public override void UpdateState(float deltaTime)
+    {
+        if(isPreparing)
+        {
+            LookAtPlayer(deltaTime);
+            return;
+        }
+
+        if(isCharging)
+        {
+            stateMachine.GetEnemyTransform().position = Vector3.MoveTowards(stateMachine.GetEnemyTransform().position, fixedTarget, deltaTime*chargeStrenght);
+            if(stateMachine.GetEnemyTransform().position == fixedTarget)
+            {
+                isCharging = false;
+                stateMachine.StartCoroutine("WaitTime", 1f);
+            }
+            return;
+        }
+
+        if(isResting)
+        {
+            return;
+        }
+
+        stateMachine.ChangeToState(idleStateKey);
+    }
+
+    public override void ExitState()
+    {
+        stateMachine.waitForTime.RemoveListener(OnWaitOver);
+    }
+
+    public void LookAtPlayer(float deltaTime)
+    {
+        //Debug.Log("look");
+        Transform newTarget = activePlayers[0].transform;
+
+        for(int i = 1; i < activePlayers.Length; i++)
+        {
+            if(Vector3.Distance(activePlayers[i].transform.position, stateMachine.GetEnemyTransform().position)
+                < Vector3.Distance(newTarget.position, stateMachine.GetEnemyTransform().position))
+            {
+                newTarget = activePlayers[i].transform;
+            }
+        }
+
+        Vector3 initialAngle = stateMachine.GetEnemyTransform().eulerAngles;
+        stateMachine.GetEnemyTransform().LookAt(newTarget);
+        Vector3 targetAngle = new Vector3(0, stateMachine.GetEnemyTransform().eulerAngles.y, 0);
+        float angle = Mathf.MoveTowardsAngle(initialAngle.y, targetAngle.y, deltaTime*210f);
+        stateMachine.GetEnemyTransform().eulerAngles = new Vector3(0, angle, 0);
+    }
+
+    public void OnWaitOver()
+    {
+        if(isPreparing)
+        {
+            fixedTarget = new Vector3(targetedPlayer.position.x, stateMachine.GetEnemyTransform().position.y, targetedPlayer.position.z);
+            isPreparing = false;
+        }
+        else
+        {
+            isResting = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/States/IAs_RandomWander.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAs_RandomWander<EnemyState> : IAs_Enemy_State<EnemyState> where EnemyState : Enum
+{
+    public IAs_RandomWander(EnemyState idle, EnemyState charge, EnemyState spin) : base(idle)
+    {
+        chargeStateKey = charge;
+        spinStateKey = spin;
+    }
+
+    protected EnemyState chargeStateKey;
+    protected EnemyState spinStateKey;
+    protected GameObject[] activePlayers;
+    protected int numberOfWanders;
+    protected Vector3 targetPosition;
+    protected bool hasEnoughTimePassed, awaitingNext;
+
+    public override void EnterState(IAs_Enemy_State_Machine<EnemyState> SM)
+    {
+        stateMachine = SM;
+        activePlayers = stateMachine.GetActivePlayers();
+        numberOfWanders = UnityEngine.Random.Range(2, 5);
+        targetPosition = GetNewPosition(stateMachine.GetEnemyTransform().position);
+        hasEnoughTimePassed = false;
+        awaitingNext = false;
+        stateMachine.waitForTime.AddListener(OnWaitOver);
+        stateMachine.StartCoroutine("WaitTime", 2f);
+        Debug.Log(numberOfWanders);
+    }
+
+    public override void UpdateState(float deltaTime)
+    {
+        LookAtPlayer(deltaTime);
+
+        if(awaitingNext)
+        {
+            return;
+        }
+        
+        if(numberOfWanders == 0)
+        {
+            if(UnityEngine.Random.Range(0, 3) == 0)
+            {
+                stateMachine.ChangeToState(spinStateKey);
+            }
+            else
+            {
+                stateMachine.ChangeToState(chargeStateKey);
+            }
+            return;
+        }
+        
+        if(stateMachine.GetEnemyTransform().position != targetPosition && hasEnoughTimePassed == false)
+        {
+            stateMachine.GetEnemyTransform().position = Vector3.MoveTowards(stateMachine.GetEnemyTransform().position, targetPosition, deltaTime*3f);
+            return;
+        }
+
+        stateMachine.StopAllCoroutines();
+        hasEnoughTimePassed = true;
+        awaitingNext = true;
+        numberOfWanders--;
+        stateMachine.StartCoroutine("WaitTime", 1.5f);
+    }
+
+    public override void ExitState()
+    {
+        numberOfWanders = 0;
+        awaitingNext = false;
+        hasEnoughTimePassed = false;
+        stateMachine.waitForTime.RemoveListener(OnWaitOver);
+    }
+
+    private Vector3 GetNewPosition(Vector3 currentPosition)
+    {
+        float newX = UnityEngine.Random.Range(0, 5)-2;
+        float newZ = UnityEngine.Random.Range(0, 5)-2;
+
+        while(currentPosition.x + newX > 7 || currentPosition.x + newX < -7)
+        {
+            newX = UnityEngine.Random.Range(0, 5)-2;
+        }
+        while(currentPosition.z + newZ > 7 || currentPosition.z + newZ < -7)
+        {
+            newZ = UnityEngine.Random.Range(0, 5)-2;
+        }
+
+        return new Vector3(currentPosition.x+newX, currentPosition.y, currentPosition.z+newZ);
+    }
+
+    public void LookAtPlayer(float deltaTime)
+    {
+        //Debug.Log("look");
+        Transform newTarget = activePlayers[0].transform;
+
+        for(int i = 1; i < activePlayers.Length; i++)
+        {
+            if(Vector3.Distance(activePlayers[i].transform.position, stateMachine.GetEnemyTransform().position)
+                < Vector3.Distance(newTarget.position, stateMachine.GetEnemyTransform().position))
+            {
+                newTarget = activePlayers[i].transform;
+            }
+        }
+
+        Vector3 initialAngle = stateMachine.GetEnemyTransform().eulerAngles;
+        stateMachine.GetEnemyTransform().LookAt(newTarget);
+        Vector3 targetAngle = new Vector3(0, stateMachine.GetEnemyTransform().eulerAngles.y, 0);
+        float angle = Mathf.MoveTowardsAngle(initialAngle.y, targetAngle.y, deltaTime*210f);
+        stateMachine.GetEnemyTransform().eulerAngles = new Vector3(0, angle, 0);
+    }
+
+    public void OnWaitOver()
+    {
+        if(awaitingNext)
+        {
+            awaitingNext = false;
+            hasEnoughTimePassed = false;
+            targetPosition = GetNewPosition(stateMachine.GetEnemyTransform().position);
+        }
+        else
+        {
+            hasEnoughTimePassed = true;
+        }
+    }
+}
