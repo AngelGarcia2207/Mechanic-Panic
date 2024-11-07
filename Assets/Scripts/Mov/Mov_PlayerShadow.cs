be449939d4d437b9babf133fe62c1b0dc7f6311b
AlejandroA00833118 <102173423+AlejandroA00833118@users.noreply.github.com>
2024-11-07 00:34:35 -0600

    sombra debajo del jugador

--- Assets/Scripts/Mov/Mov_PlayerShadow.cs
+++ Assets/Scripts/Mov/Mov_PlayerShadow.cs
@@ -2,28 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shadow : MonoBehaviour
+public class Mov_PlayerShadow : MonoBehaviour
 {
     [SerializeField] private Transform sourceTransform;
     [SerializeField] private float maxRaycastDistance;
-
     [SerializeField] private float scaleMultiplier = 0.1f;
     private Vector3 originalScale;
-    private float originalDistance;
+    private Vector3 minScale;
 
     void Start()
     {
         originalScale = transform.localScale;
-        originalDistance = (transform.position - sourceTransform.position).magnitude;
+        minScale = originalScale * scaleMultiplier;
     }
-    
 
     void Update()
     {
         float distance = raycastFloorDistance();
         
-        transform.position = sourceTransform.position - new Vector3(0, distance, 0);
-        // transform.localScale =
+        transform.position = sourceTransform.position - new Vector3(0, distance - 0.01f, 0);
+        transform.localScale = Vector3.Lerp(originalScale, minScale, distance / maxRaycastDistance);
     }
 
     private float raycastFloorDistance() {
@@ -31,12 +29,14 @@ public class Shadow : MonoBehaviour
         RaycastHit hit;
         Vector3 direction = -transform.up;
 
-        if (Physics.Raycast(origin, direction, out hit, maxRaycastDistance)) {
+        int layerMask = LayerMask.GetMask("Default");
+
+        if (Physics.Raycast(origin, direction, out hit, maxRaycastDistance, layerMask)) {
             float distance = (hit.point - origin).magnitude;
             return distance;
         }
         else {
-            return float.MaxValue;
+            return maxRaycastDistance;
         }
     }
 }
