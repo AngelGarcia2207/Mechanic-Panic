b9b7c623cb5c58fb0e1ff0c468f5174f99356107
GabrielRuizMiller <110938299+GabrielRuizMiller@users.noreply.github.com>
2024-11-22 09:54:19 -0600

    Revert "Merge pull request #151 from AngelGarcia2207/Alejandro"
    This reverts commit 4405f52d9b3feef9391c5178d2488eb79c293741, reversing
    changes made to 129e148868042ad72afd8f5ce03f08dcd0747c6f.

---
+++ Assets/Scripts/UI/UI_Play_Selection.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Play_Selection : MonoBehaviour
+{
+    [SerializeField] private UI_SelectionCard[] selectionCards;
+    [SerializeField] private GameObject characterSelectionMenu, chooseTextObj;
+    private int enteredCursors;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Play()
+    {
+        int playersAsigned = 0;
+        foreach (UI_SelectionCard card in selectionCards)
+        {
+            if (card.asignedCursor != null)
+            {
+                playersAsigned++;
+            }
+        }
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        if(playersAsigned == players.Length)
+        {
+            foreach (GameObject player in players)
+            {
+                player.GetComponent<Mov_Player_Controller>().finishedSelection = true;
+            }
+
+            characterSelectionMenu.SetActive(false);
+        }
+        else
+        {
+            chooseTextObj.SetActive(false);
+            chooseTextObj.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D tri)
+    {
+        if (tri.gameObject.name == "Cursor")
+        {
+            enteredCursors++;
+
+            UI_Cursor cursor = tri.gameObject.GetComponent<UI_Cursor>();
+            Mov_Player_Controller playerController = cursor.playerController;
+
+            cursor.hoveredPlayButton = this;
+
+            animator.SetBool("hovering", true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D tri)
+    {
+        if (tri.gameObject.name == "Cursor")
+        {
+            enteredCursors--;
+
+            UI_Cursor cursor = tri.gameObject.GetComponent<UI_Cursor>();
+            Mov_Player_Controller playerController = cursor.playerController;
+
+            cursor.hoveredPlayButton = null;
+
+            if (enteredCursors == 0)
+            {
+                animator.SetBool("hovering", false);
+            }
+        }
+    }
+}
